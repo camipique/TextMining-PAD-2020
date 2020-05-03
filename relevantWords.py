fc5c7f53e4ed967dfdbcaba61fe78020517a77cc
DavidPais <dm.pais@campus.fct.unl.pt>
2020-05-03 13:27:49 +0100

    Update relevantWords.py
    OmegaMinus1 and OmegaPlus1 created. Correct glues with value 1 and optimize performance

--- relevantWords.py
+++ relevantWords.py
@@ -9,11 +9,10 @@ PAD Project - Text Mining
 # Part 1 - Extracting relevant words
 
 import re, os
-import numpy as np
 from nltk import FreqDist
 from nltk.util import everygrams, ngrams
 
-CORPUS_FOLDER_PATH = "corpus2mw/"
+CORPUS_FOLDER_PATH = "corpus2mwTest/"
 
 def find_n_grams(text):
     words = list(ngrams(text, 1))
@@ -49,6 +48,7 @@ for file_name in os.listdir(CORPUS_FOLDER_PATH):
     
     with open(CORPUS_FOLDER_PATH + file_name, "r", encoding="utf8") as f:
         text = f.read()
+        #print(len(text))
         # find the regex defined in text
         text_list = re.findall(regex, text)
          
@@ -180,24 +180,75 @@ glues = {
         6: {}
 }
 
+omegaMinus1 = {
+        1: {},
+        2: {},
+        3: {},
+        4: {},
+        5: {},
+        6: {}
+}
+
+
+omegaPlus1 = {
+        0: {},
+        1: {},
+        2: {},
+        3: {},
+        4: {},
+        5: {}
+}
+
+
+
+
 word_count = len(words)
 
-def n_gram_prob(n_gram):
+def n_gram_prob(n_gram): # [dictionary entry][element inside the dictionary entry]
     return n_grams_freq[len(n_gram) - 1][n_gram] / word_count
 
 def glue(n_gram):
     
-    f = 0
-    
-    for i in range(1, len(n_gram)):
+    f = 0 #frequency
+
+    if len(n_gram) == 1:
+        return n_gram_prob(n_gram)
+
+    for i in range(1, len(n_gram)): # starting in 1 because :1 goes till the start index, so starts in 0. i: starts in the index (1) till the index
         f += n_gram_prob( n_gram[:i] ) * n_gram_prob( n_gram[i:] )
-        
-    f = f / ( len(n_gram) - 1 )
+
+
+    f = f / ( len(n_gram) - 1 ) # formula of F 
+
     
-    return n_gram_prob(n_gram) ** 2 / f
+    return n_gram_prob(n_gram) ** 2 / f # formula of glue of a n-gram with n > 2 (it can also apply to n = 2)
     
-for i in range(1, 7):
-    for n_gram in n_grams[i]:
+
+# main method
+
+for i in range(6, 0, -1): # starts in 1 till 6 (inclusive)
+    for n_gram in n_grams[i]: # in dictionary entry, and inside that entry glues[i][ngram] calculate the glues
         glues[i][n_gram] = glue(n_gram)
-
-
+        omegaMinus1[i][n_gram] = [glue(n_gram[1:])]  
+        omegaMinus1[i][n_gram].append( glue(n_gram[ :i]) )
+        
+      #  omegaPlus1[i - 1][n_gram[1:]] = []  
+        #omegaPlus1[i - 1][n_gram[1:]].append(glue(n_gram))
+        
+        try: # n_gram[1:] in omegaPlus1[i-1]:
+            omegaPlus1[i-1][n_gram[1:]].append(glue(n_gram))
+        
+        except KeyError:
+             omegaPlus1[i-1][n_gram[1:]] = [glue(n_gram)]
+        
+        try: # n_gram[:i] in omegaPlus1[i-1]:
+            omegaPlus1[i-1][n_gram[:i]].append(glue(n_gram))
+        
+        except KeyError:
+             omegaPlus1[i-1][n_gram[:i]] = [glue(n_gram)]
+        
+        
+  #      omegaPlus1[i - 1][n_gram[:i]] = []
+      #  omegaPlus1[i - 1][n_gram[:i]].append(glue(n_gram))
+        
+  
